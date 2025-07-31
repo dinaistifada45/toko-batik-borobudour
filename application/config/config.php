3f3823c0fa782ca980396adf2dd0616041f026e3
ulum <apumm12@gmail.com>
2025-07-31 20:54:36 +0700

    test

--- application/config/config.php
+++ application/config/config.php
@@ -25,7 +25,6 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 */
 $config['base_url'] = 'http://localhost/Toko_Batik_Borobudour/';
 
-
 /*
 |--------------------------------------------------------------------------
 | Index File
